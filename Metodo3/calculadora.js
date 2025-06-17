8fc12225541be6ce6a67a29afd81369efe411f36
thiagomenin5 <tatimenin50@gmail.com>
2025-06-17 12:11:02 -0300

    correccion de calculadora metodo restar

--- Metodo3/calculadora.js
+++ Metodo3/calculadora.js
@@ -5,7 +5,7 @@ function sumar(num1,num2){
     return num1 + num2;
 }
 function restar(num1,num2){
-    return num1 + num2;
+    return num1 - num2;
 }
 function multiplicar(num1,num2){
     return num1 * num2;
